f829353cf834d5845fc882dc22dab2f8341e982e
Anurag Shirolkar <ashirolkar@ashirolkar-macbookpro1.roam.corp.google.com>
2019-10-13 14:33:43 +0800

    added turning horizontal

--- 3d-project/src/Camera.hs
+++ 3d-project/src/Camera.hs
@@ -23,11 +23,6 @@ goBack dist cam = translate (0,0,(-dist)) cam
 goUp :: Float -> Camera -> Camera
 goUp dist cam = translate (0,dist,0) cam
 
-translate :: Point3 -> Camera -> Camera
-translate (x,y,z) (Camera mat) = Camera $ Matrix.multStd newMat mat
-    where
-        newMat = Matrix.fromList 4 4 [1,0,0,(-x), 0,1,0,(-y), 0,0,1,(-z), 0,0,0,1]
-
 sampleCam :: Camera
 sampleCam = goUp 100 $ goBack 1000 $ (Camera (Matrix.identity 4))
 
@@ -37,7 +32,22 @@ makeImage (Camera mat) (x,y,z) = (x'/z', y'/z')
         pointMat = Matrix.fromList 4 1 [x, y, z, 1]
         [x',y',z'] = Vector.toList $ Matrix.getCol 1 $ Matrix.multStd perspectiveM (Matrix.multStd mat pointMat)
 
-translateUnit = 10
+
+
+translate :: Point3 -> Camera -> Camera
+translate (x,y,z) (Camera mat) = Camera $ Matrix.multStd newMat mat
+    where
+        newMat = Matrix.fromList 4 4 [1,0,0,(-x), 0,1,0,(-y), 0,0,1,(-z), 0,0,0,1]
+
+-- angle in radians, positive when camera turns left
+turnHorizontal :: Float -> Camera -> Camera
+turnHorizontal theta (Camera mat) = Camera $ Matrix.multStd newMat mat
+    where
+        newMat = Matrix.fromList 4 4 [cos theta,0,sin theta,0, 0,1,0,0, -(sin theta),0,cos theta,0, 0,0,0,1]
+
+
+translateUnit = 5
+turnUnit = 0.01
 
 makeMovement :: Camera -> Movement -> Camera
 makeMovement cam Events.MoveForward = translate (0,0,translateUnit) cam
@@ -46,4 +56,6 @@ makeMovement cam Events.MoveLeft = translate (-translateUnit,0,0) cam
 makeMovement cam Events.MoveRight = translate (translateUnit,0,0) cam
 makeMovement cam Events.MoveUp = translate (0,translateUnit,0) cam
 makeMovement cam Events.MoveDown = translate (0,-translateUnit,0) cam
+makeMovement cam Events.TurnLeft = turnHorizontal turnUnit cam
+makeMovement cam Events.TurnRight = turnHorizontal (-turnUnit) cam
 makeMovement cam _ = cam
--- 3d-project/src/Events.hs
+++ 3d-project/src/Events.hs
@@ -12,10 +12,10 @@ data Movement =
     | MoveRight
     | MoveUp
     | MoveDown
-    | PanRight
-    | PanLeft
-    | PanUp
-    | PanDown
+    | TurnRight
+    | TurnLeft
+    | TurnUp
+    | TurnDown
     | TiltRight
     | TiltLeft
     deriving (Read, Show, Eq, Ord, Enum, Bounded)
@@ -25,15 +25,27 @@ data Action = Add Movement | Remove Movement | Reset | NoAction
 fromEvent :: Event -> Action
 fromEvent (EventKey (SpecialKey KeyUp) Down _ _) = Add MoveForward
 fromEvent (EventKey (SpecialKey KeyUp) Up _ _) = Remove MoveForward
+
 fromEvent (EventKey (SpecialKey KeyDown) Down _ _) = Add MoveBackward
 fromEvent (EventKey (SpecialKey KeyDown) Up _ _) = Remove MoveBackward
-fromEvent (EventKey (SpecialKey KeyLeft) Down _ _) = Add MoveLeft
-fromEvent (EventKey (SpecialKey KeyLeft) Up _ _) = Remove MoveLeft
-fromEvent (EventKey (SpecialKey KeyRight) Down _ _) = Add MoveRight
-fromEvent (EventKey (SpecialKey KeyRight) Up _ _) = Remove MoveRight
-fromEvent (EventKey (Char 'j') Down _ _) = Add MoveDown
-fromEvent (EventKey (Char 'j') Up _ _) = Remove MoveDown
-fromEvent (EventKey (Char 'k') Down _ _) = Add MoveUp
-fromEvent (EventKey (Char 'k') Up _ _) = Remove MoveUp
+
+fromEvent (EventKey (SpecialKey KeyLeft) Down _ _) = Add TurnLeft
+fromEvent (EventKey (SpecialKey KeyLeft) Up _ _) = Remove TurnLeft
+
+fromEvent (EventKey (SpecialKey KeyRight) Down _ _) = Add TurnRight
+fromEvent (EventKey (SpecialKey KeyRight) Up _ _) = Remove TurnRight
+
+fromEvent (EventKey (Char 'a') Down _ _) = Add MoveLeft
+fromEvent (EventKey (Char 'a') Up _ _) = Remove MoveLeft
+
+fromEvent (EventKey (Char 'd') Down _ _) = Add MoveRight
+fromEvent (EventKey (Char 'd') Up _ _) = Remove MoveRight
+
+fromEvent (EventKey (Char 's') Down _ _) = Add MoveDown
+fromEvent (EventKey (Char 's') Up _ _) = Remove MoveDown
+
+fromEvent (EventKey (Char 'w') Down _ _) = Add MoveUp
+fromEvent (EventKey (Char 'w') Up _ _) = Remove MoveUp
+
 fromEvent (EventKey (Char 'r') Down _ _) = Reset
 fromEvent _ = NoAction
